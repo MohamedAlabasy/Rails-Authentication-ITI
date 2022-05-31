4a3d40a1b6ee2c7bb4f8bf4c0c5b6ac012329b45
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-05-31 08:05:23 +0200

    add Controller

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,13 @@
+class AuthController < ApplicationController
+  def signup_form
+  end
+
+  def signup
+  end
+
+  def signin_form
+  end
+
+  def signin
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'registration' , to: 'auth#signup_form'
+  get 'registration' , controller:'auth', action:'signup_form'
+  post 'signup', to: 'auth#signup', as:'signup'
+  get 'login' ,to:'auth#signin_form' 
+  post 'signin' ,to: 'auth#signin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
